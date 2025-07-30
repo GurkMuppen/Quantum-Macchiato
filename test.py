2b8cc52803622bdbc516e8d0a582c94f4c371b2b
Henrik <henrik@waldeck.se>
2025-07-30 15:46:59 +0200

    Small changes

--- test.py
+++ test.py
@@ -1,16 +1,3 @@
-import subprocess
+from qetool import *
 
-with open("test.out", "w") as outfile:
-    process = subprocess.Popen(
-        ["conda", "run", "-n", "qespresso", "pw.x", "-in", "test.in"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        universal_newlines=True,
-        bufsize=1
-    )
-    for line in process.stdout:
-        outfile.write(line)
-        outfile.flush()
-    process.wait()
-
-print("Hello World!")
+run_pw_simulation()
